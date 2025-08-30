6026f1c40a133221e5fb8889fcc937fab2fe5c7f
Jaime <jaimet7936824@gmail.com>
2025-08-29 19:40:08 -0500

    Avance de Product

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,14 @@ import Cart from "./pages/Cart.jsx";
 import Product from "./pages/Product";
 import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx"
+import SearchBar from "./components/SearchBar.jsx";
 
 const App = () => {
   return (
     <div>
 <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
         <Navbar />
+        <SearchBar/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/collection" element={<Collection />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, {  useContext, useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, NavLink } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
+
+  const {setShowSearch}  = useContext(ShopContext)
+  
 
   return (
     <div className="flex items-center justify-between py-5 font-medium relative">
@@ -34,7 +38,7 @@ const Navbar = () => {
 
       {/* Icons */}
       <div className="flex items-center gap-6">
-        <img
+        <img onClick={()=>setShowSearch(true)}
           src={assets.search_icon}
           className="w-5 cursor-pointer"
           alt="Search"
